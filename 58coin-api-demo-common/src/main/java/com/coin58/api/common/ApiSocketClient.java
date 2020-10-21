7231a9bc4c2f8a05e591334bed00fd3f81a3f059
Partija <122193466+Partija@users.noreply.github.com>
2020-10-21 12:38:48 +0000

    Version 0.2.2

--- 58coin-api-demo-common/src/main/java/com/coin58/api/common/ApiSocketClient.java
+++ 58coin-api-demo-common/src/main/java/com/coin58/api/common/ApiSocketClient.java
@@ -21,3 +21,39 @@ public interface ApiSocketClient {
      *
      * @param products market product(s) number (one or coma-separated) to subscribe to
      * @param callback the callback to call on new events
+     * @return a {@link Closeable} that allows the underlying web socket to be closed.
+     */
+    Closeable onDepthEvent(String products, ApiCallback<DepthEvent> callback);
+
+    /**
+     * Open a new web socket to receive {@link KlineEvent klineEvents} on a callback.
+     *
+     * @param products market product(s) number (one or coma-separated) to subscribe to
+     * @param interval the interval of the candles tick events required
+     * @param callback the callback to call on new events
+     * @return a {@link Closeable} that allows the underlying web socket to be closed.
+     */
+    Closeable onKlineEvent(String products, KlineIntervalEnum interval, ApiCallback<KlineEvent> callback);
+
+    /**
+     * Open a new web socket to receive {@link TickerEvent tickerEvents} on a callback.
+     *
+     * @param products market product(s) number (one or coma-separated) to subscribe to
+     * @param callback the callback to call on new events
+     * @return a {@link Closeable} that allows the underlying web socket to be closed.
+     */
+    Closeable onTickerEvent(String products, ApiCallback<TickerEvent> callback);
+
+    /**
+     * Open a new web socket to receive {@link OrderUpdateEvent orderUpdateEvents} on a callback.
+     *
+     * @param businesses the business name of subscription.
+     * @param callback   the callback to call on new events
+     * @return a {@link Closeable} that allows the underlying web socket to be closed.
+     */
+    Closeable onOrderUpdateEvent(String businesses, ApiCallback<OrderUpdateEvent> callback);
+
+
+    Closeable createWebSocket(String channel, WebSocketListener webSocketListener);
+
+}
