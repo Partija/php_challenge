7b7f26836ba510c4c638601acc67ccd89bcfc4c2
Partija <122193466+Partija@users.noreply.github.com>
2020-10-23 09:31:40 +0000

    adding example

--- 58coin-api-demo-common/src/main/java/com/coin58/api/common/ConverterFactory.java
+++ 58coin-api-demo-common/src/main/java/com/coin58/api/common/ConverterFactory.java
@@ -2,4 +2,7 @@ package com.coin58.api.common;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.
+import com.alibaba.fastjson.JSONObject;
+import lombok.extern.slf4j.Slf4j;
+import okhttp3.ResponseBody;
+import
