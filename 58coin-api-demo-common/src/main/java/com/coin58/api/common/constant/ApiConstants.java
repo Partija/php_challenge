40e2473e1f031c313e30e252b98d9df5c9bae6bd
Partija <122193466+Partija@users.noreply.github.com>
2020-11-05 11:13:36 +0000

    Add thumbnail

--- 58coin-api-demo-common/src/main/java/com/coin58/api/common/constant/ApiConstants.java
+++ 58coin-api-demo-common/src/main/java/com/coin58/api/common/constant/ApiConstants.java
@@ -3,4 +3,9 @@ package com.coin58.api.common.constant;
 /**
  * Constants used throughout 58coin's API.
  */
-public 
+public interface ApiConstants {
+
+    /**
+     * REST API base URL.
+     */
+    Strin
