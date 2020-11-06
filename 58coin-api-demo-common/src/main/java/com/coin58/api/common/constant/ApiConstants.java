24766a45028b6df104ab58c8b2f0c353721ae439
Partija <122193466+Partija@users.noreply.github.com>
2020-11-06 14:49:00 +0000

    Update no_excel.md

--- 58coin-api-demo-common/src/main/java/com/coin58/api/common/constant/ApiConstants.java
+++ 58coin-api-demo-common/src/main/java/com/coin58/api/common/constant/ApiConstants.java
@@ -9,4 +9,9 @@ public interface ApiConstants {
      * REST API base URL.
      */
     String REST_API_BASE_URL = "https://openapi.58ex.com";
-//    String REST_API_BASE_URL = "http://
+//    String REST_API_BASE_URL = "http://192.168.112.23:9994";
+
+    /**
+     * Streaming API base URL.
+     */
+    String WS_API
