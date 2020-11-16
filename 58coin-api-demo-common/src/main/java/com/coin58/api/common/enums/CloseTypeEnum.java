c86057951d64da9013d2193d9cc6be22548082ce
Partija <122193466+Partija@users.noreply.github.com>
2020-11-16 21:49:26 +0000

    more cases

--- 58coin-api-demo-common/src/main/java/com/coin58/api/common/enums/CloseTypeEnum.java
+++ 58coin-api-demo-common/src/main/java/com/coin58/api/common/enums/CloseTypeEnum.java
@@ -2,4 +2,12 @@ package com.coin58.api.common.enums;
 
 public enum CloseTypeEnum {
 
-    OPEN(0), CL
+    OPEN(0), CLOSE(1);
+
+    int code;
+
+    CloseTypeEnum(int code) {
+        this.code = code;
+    }
+
+  
