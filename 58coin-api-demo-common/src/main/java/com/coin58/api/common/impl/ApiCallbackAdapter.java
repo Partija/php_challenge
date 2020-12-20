3c0751ac929138162dd1969830ff61cb1b8716cf
Partija <122193466+Partija@users.noreply.github.com>
2020-12-20 13:30:04 +0000

    sed

--- 58coin-api-demo-common/src/main/java/com/coin58/api/common/impl/ApiCallbackAdapter.java
+++ 58coin-api-demo-common/src/main/java/com/coin58/api/common/impl/ApiCallbackAdapter.java
@@ -2,4 +2,6 @@ package com.coin58.api.common.impl;
 
 import com.coin58.api.common.ApiCallback;
 import com.coin58.api.common.RestResult;
-import com.coin58.api.comm
+import com.coin58.api.common.exception.ApiException;
+import retrofit2.Call;
+import retrofit2.Callback;
