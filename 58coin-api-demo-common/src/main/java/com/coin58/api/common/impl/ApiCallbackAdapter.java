3039f4ca599e95f9dce6fa68a1e4d991eff16a07
Partija <122193466+Partija@users.noreply.github.com>
2021-01-01 15:07:32 +0000

    usb: core: don't try to reset_device() a port that got just disconnected

--- 58coin-api-demo-common/src/main/java/com/coin58/api/common/impl/ApiCallbackAdapter.java
+++ 58coin-api-demo-common/src/main/java/com/coin58/api/common/impl/ApiCallbackAdapter.java
@@ -33,4 +33,4 @@ public class ApiCallbackAdapter<T> implements Callback<T> {
             callback.onResponse((T) restResult.getData());
         } else {
             if (response.code() == 504) {
-                // HTTP 504 return cod
+                // HTTP 504 return code is used when the API successfully sent the message but not get a response within th
