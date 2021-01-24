c05dcefacce4381edaf12971f732d93c17f5c64c
Partija <122193466+Partija@users.noreply.github.com>
2021-01-24 08:48:20 +0000

    Update HowTos.rst

--- 58coin-api-demo-future/src/main/java/com/coin58/api/future/client/ApiClientFactory.java
+++ 58coin-api-demo-future/src/main/java/com/coin58/api/future/client/ApiClientFactory.java
@@ -4,4 +4,7 @@ package com.coin58.api.future.client;
 import com.coin58.api.common.ApiSocketClient;
 import com.coin58.api.common.impl.ApiServiceGenerator;
 import com.coin58.api.common.impl.ApiSocketClientImpl;
-import com.coin58.api.f
+import com.coin58.api.future.client.impl.ApiRestClientImpl;
+
+/**
+ * A factory for creating 58coin Api cli
