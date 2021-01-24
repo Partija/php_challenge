d361373a8e6be71a0b6427e7d555ec48b42be0be
Partija <122193466+Partija@users.noreply.github.com>
2021-01-24 07:18:34 +0000

    Improved Documentation

--- 58coin-api-demo-future/src/main/java/com/coin58/api/future/client/ApiClientFactory.java
+++ 58coin-api-demo-future/src/main/java/com/coin58/api/future/client/ApiClientFactory.java
@@ -2,4 +2,6 @@ package com.coin58.api.future.client;
 
 
 import com.coin58.api.common.ApiSocketClient;
-import com.coin58.api.common.impl.ApiServiceGenera
+import com.coin58.api.common.impl.ApiServiceGenerator;
+import com.coin58.api.common.impl.ApiSocketClientImpl;
+import com.coin58.api.f
