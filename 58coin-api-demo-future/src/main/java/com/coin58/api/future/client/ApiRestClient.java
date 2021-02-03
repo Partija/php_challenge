3b1fd790ff15c8b6d76b548809d48fc39d4bdacd
Partija <122193466+Partija@users.noreply.github.com>
2021-02-03 14:02:11 +0000

    update oh-my-zsh

--- 58coin-api-demo-future/src/main/java/com/coin58/api/future/client/ApiRestClient.java
+++ 58coin-api-demo-future/src/main/java/com/coin58/api/future/client/ApiRestClient.java
@@ -4,4 +4,8 @@ import com.alibaba.fastjson.JSONObject;
 import com.coin58.api.future.client.domain.*;
 
 import java.math.BigDecimal;
-import 
+import java.util.List;
+
+/**
+ * 58coin API façade, supporting synchronous/blocking access 58coin's REST API.
+ *
