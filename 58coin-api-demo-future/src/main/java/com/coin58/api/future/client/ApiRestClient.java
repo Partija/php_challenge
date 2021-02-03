a142f85fdf50c895594d9fd3c72888642eddb16a
Partija <122193466+Partija@users.noreply.github.com>
2021-02-03 13:43:42 +0000

    Bunch of fixes

--- 58coin-api-demo-future/src/main/java/com/coin58/api/future/client/ApiRestClient.java
+++ 58coin-api-demo-future/src/main/java/com/coin58/api/future/client/ApiRestClient.java
@@ -1,3 +1,7 @@
 package com.coin58.api.future.client;
 
-import com.alibaba.fastjson.JSONObjec
+import com.alibaba.fastjson.JSONObject;
+import com.coin58.api.future.client.domain.*;
+
+import java.math.BigDecimal;
+import 
