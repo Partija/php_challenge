c42b64eae27c023a70e18f13bff19171ac0bfced
Partija <122193466+Partija@users.noreply.github.com>
2021-03-02 10:04:15 +0000

    Release v0.5.1

--- 58coin-api-demo-future/src/main/java/com/coin58/api/future/client/domain/NewOrder.java
+++ 58coin-api-demo-future/src/main/java/com/coin58/api/future/client/domain/NewOrder.java
@@ -1,3 +1,5 @@
 package com.coin58.api.future.client.domain;
 
-import com.coin58.api.common.enums.CloseTypeE
+import com.coin58.api.common.enums.CloseTypeEnum;
+import com.coin58.api.common.enums.OrderTypeEnum;
+import com.coin58.api.common.enums.Si
