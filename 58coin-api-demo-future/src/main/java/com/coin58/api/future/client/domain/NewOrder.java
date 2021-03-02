85fd9c5fda4ff1e9aa6c0fa3c09daed16f629363
Partija <122193466+Partija@users.noreply.github.com>
2021-03-02 12:28:41 +0000

    Ignore build directory

--- 58coin-api-demo-future/src/main/java/com/coin58/api/future/client/domain/NewOrder.java
+++ 58coin-api-demo-future/src/main/java/com/coin58/api/future/client/domain/NewOrder.java
@@ -2,4 +2,10 @@ package com.coin58.api.future.client.domain;
 
 import com.coin58.api.common.enums.CloseTypeEnum;
 import com.coin58.api.common.enums.OrderTypeEnum;
-import com.coin58.api.common.enums.Si
+import com.coin58.api.common.enums.SideEnum;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class NewOrder {
