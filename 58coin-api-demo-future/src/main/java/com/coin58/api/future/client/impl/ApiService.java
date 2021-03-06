71d8c18cf58a6242880d926bd843b3d9a0f16118
Partija <122193466+Partija@users.noreply.github.com>
2021-03-06 18:29:38 +0000

    chore(release): 1.1.2

--- 58coin-api-demo-future/src/main/java/com/coin58/api/future/client/impl/ApiService.java
+++ 58coin-api-demo-future/src/main/java/com/coin58/api/future/client/impl/ApiService.java
@@ -1,4 +1,6 @@
 package com.coin58.api.future.client.impl;
 
 import com.alibaba.fastjson.JSONObject;
-import com.coin
+import com.coin58.api.common.constant.ApiConstants;
+import com.coin58.api.future.client.domain.*;
+im
