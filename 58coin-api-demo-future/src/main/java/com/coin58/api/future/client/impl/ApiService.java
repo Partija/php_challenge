a6e0a643d83af8a18184ef94f2248f2ee27e8486
Partija <122193466+Partija@users.noreply.github.com>
2021-04-10 18:12:13 +0000

    fix size

--- 58coin-api-demo-future/src/main/java/com/coin58/api/future/client/impl/ApiService.java
+++ 58coin-api-demo-future/src/main/java/com/coin58/api/future/client/impl/ApiService.java
@@ -67,4 +67,6 @@ public interface ApiService {
     @Headers({ApiConstants.HEADER_CONTENT_TYPE_JSON, ApiConstants.ENDPOINT_HEADER_API_KEY})
     @GET("/v1/usdt/order/list")
     Call<List<OrderListResponse>> getOrderList(@Query("contractIds") String contractIds, @Query("side") Integer side,
-                                               @Que
+                                               @Query("close") Integer close);
+
+    @Headers({ApiConstants.HEADER_CONTENT_TYPE_JS
