1f8aebe493052e8ffffca2bfbf5a575695ce54da
Partija <122193466+Partija@users.noreply.github.com>
2021-04-11 14:53:04 +0000

    v 0.0.1

--- 58coin-api-demo-future/src/main/java/com/coin58/api/future/client/impl/ApiService.java
+++ 58coin-api-demo-future/src/main/java/com/coin58/api/future/client/impl/ApiService.java
@@ -71,4 +71,5 @@ public interface ApiService {
 
     @Headers({ApiConstants.HEADER_CONTENT_TYPE_JSON, ApiConstants.ENDPOINT_HEADER_API_KEY})
     @GET("/v1/usdt/order/fill/list")
-    Call<List<FillListResponse>> getFillList(@Query("contractIds") String contractI
+    Call<List<FillListResponse>> getFillList(@Query("contractIds") String contractIds, @Query("close") Integer close,
+                                             @Query("limit") 
