62dc41cec4ef6010a60b7cf004c6cfa7c22f7f15
Partija <122193466+Partija@users.noreply.github.com>
2021-04-12 18:43:09 +0000

    Minor documentation update.

--- 58coin-api-demo-future/src/main/java/com/coin58/api/future/client/impl/ApiService.java
+++ 58coin-api-demo-future/src/main/java/com/coin58/api/future/client/impl/ApiService.java
@@ -78,4 +78,6 @@ public interface ApiService {
     @GET("/v1/usdt/order/finish")
     Call<FinishOrderResponse> getFinishOrder(@Query("orderId") long orderId);
 
-    @Headers({ApiConstants.HEADER_CONTENT_TYP
+    @Headers({ApiConstants.HEADER_CONTENT_TYPE_JSON, ApiConstants.ENDPOINT_HEADER_API_KEY})
+    @GET("/v1/usdt/position/list")
+    Call<JSONObject
