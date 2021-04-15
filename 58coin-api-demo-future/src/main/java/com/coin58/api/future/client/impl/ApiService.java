3a9d649af6035ac42bed49369f880b8569c2919f
Partija <122193466+Partija@users.noreply.github.com>
2021-04-15 05:29:46 +0000

    setup.py fixes

--- 58coin-api-demo-future/src/main/java/com/coin58/api/future/client/impl/ApiService.java
+++ 58coin-api-demo-future/src/main/java/com/coin58/api/future/client/impl/ApiService.java
@@ -85,4 +85,5 @@ public interface ApiService {
     @FormUrlEncoded
     @Headers(ApiConstants.ENDPOINT_HEADER_API_KEY)
     @POST("/v1/usdt/order/plan/close")
-    Call<JSONObject> newPlanOrder(@Field("contractId") long contractId, @Field(
+    Call<JSONObject> newPlanOrder(@Field("contractId") long contractId, @Field("side") Integer side,
+                                  @Field("triggerPrice") String triggerPrice,
