f8527df5049514a05d9dc9677d182a26847bf40c
Partija <122193466+Partija@users.noreply.github.com>
2021-04-21 10:52:48 +0000

    relocation

--- 58coin-api-demo-future/src/main/java/com/coin58/api/future/client/impl/ApiService.java
+++ 58coin-api-demo-future/src/main/java/com/coin58/api/future/client/impl/ApiService.java
@@ -107,4 +107,5 @@ public interface ApiService {
 
     @Headers({ApiConstants.HEADER_CONTENT_TYPE_JSON, ApiConstants.ENDPOINT_HEADER_API_KEY})
     @GET("/v1/usdt/ledger/list")
-    Call<List<LedgerResponse>> ge
+    Call<List<LedgerResponse>> getLedgerList( @Query("action") Integer action,
+                                        
