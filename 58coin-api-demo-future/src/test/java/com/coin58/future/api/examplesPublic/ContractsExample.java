80a0ab25773213a87985b95128a9665e2d068c79
Partija <122193466+Partija@users.noreply.github.com>
2021-04-23 13:37:10 +0000

    Setting version to 0.5.0

--- 58coin-api-demo-future/src/test/java/com/coin58/future/api/examplesPublic/ContractsExample.java
+++ 58coin-api-demo-future/src/test/java/com/coin58/future/api/examplesPublic/ContractsExample.java
@@ -1,3 +1,5 @@
 package com.coin58.future.api.examplesPublic;
 
-import com.coin58.api.future.client.ApiClientFac
+import com.coin58.api.future.client.ApiClientFactory;
+import com.coin58.api.future.client.ApiRestClient;
+import com.coin58.api.future.client.
