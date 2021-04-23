2154756ec477bc01197a13fa353c5ad04a17f482
Partija <122193466+Partija@users.noreply.github.com>
2021-04-23 15:17:46 +0000

    update player

--- 58coin-api-demo-future/src/test/java/com/coin58/future/api/examplesPublic/ContractsExample.java
+++ 58coin-api-demo-future/src/test/java/com/coin58/future/api/examplesPublic/ContractsExample.java
@@ -2,4 +2,11 @@ package com.coin58.future.api.examplesPublic;
 
 import com.coin58.api.future.client.ApiClientFactory;
 import com.coin58.api.future.client.ApiRestClient;
-import com.coin58.api.future.client.
+import com.coin58.api.future.client.domain.Contract;
+
+import java.util.List;
+
+/**
+ * @author guozhengMu
+ * @version 1.0
+ * @date 2
