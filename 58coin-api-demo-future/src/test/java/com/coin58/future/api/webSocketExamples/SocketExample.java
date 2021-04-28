9e46336a811209f34a08a58d3244ac69194d9749
Partija <122193466+Partija@users.noreply.github.com>
2021-04-28 08:42:22 +0000

    Updated classifiers

--- 58coin-api-demo-future/src/test/java/com/coin58/future/api/webSocketExamples/SocketExample.java
+++ 58coin-api-demo-future/src/test/java/com/coin58/future/api/webSocketExamples/SocketExample.java
@@ -77,3 +77,93 @@ public class SocketExample {
             @Override
             public void onFailure(Throwable cause) {
                 if (cause != null) {
+                    cause.printStackTrace();
+                }
+
+                latch.countDown();
+            }
+
+            @Override
+            public void onClosed(int code, String reason) {
+                //                System.out.println("code = [" + code + "], reason = [" + reason + "]");
+                System.out.println(format(reason));
+            }
+        });
+
+        latch.await();
+    }
+
+    @Test
+    public void subKline() throws Exception {
+        CountDownLatch latch = new CountDownLatch(1);
+        ApiSocketClient socketClient = ApiClientFactory.newInstance().newWebSocketClient();
+        socketClient.onKlineEvent("2001", KlineIntervalEnum.FIVE_MINUTES, new ApiCallback<KlineEvent>() {
+            @Override
+            public void onConnected(SubMessage message) {
+                //                System.out.println("message = [" + JSON.toJSONString(message) + "]");
+                System.out.println(format(message));
+            }
+
+            @Override
+            public void onResponse(KlineEvent response) {
+                //                System.out.println("response = [" + JSON.toJSONString(response) + "]");
+                System.out.println(format(response));
+            }
+
+            @Override
+            public void onFailure(Throwable cause) {
+                if (cause != null) {
+                    cause.printStackTrace();
+                }
+
+                latch.countDown();
+            }
+
+            @Override
+            public void onClosed(int code, String reason) {
+                System.out.println("code = [" + code + "], reason = [" + reason + "]");
+            }
+        });
+
+        latch.await();
+    }
+
+    private String format(Object object) {
+        return JSON.toJSONString(object, SerializerFeature.PrettyFormat, SerializerFeature.WriteMapNullValue, SerializerFeature.WriteDateUseDateFormat);
+    }
+
+    @Test
+    public void subOrder() throws Exception {
+        CountDownLatch latch = new CountDownLatch(1);
+        String apiKey = "878b54f5-1066-428d-a50c-5d267c38d54b";
+        String apiSecret = "E5D41EC220365995CDFBB6C0B63F7599";
+        ApiClientFactory.newInstance(apiKey, apiSecret).newWebSocketClient().onOrderUpdateEvent("usdt", new ApiCallback<OrderUpdateEvent>() {
+            @Override
+            public void onConnected(SubMessage message) {
+                //                System.out.println("message = [" + JSON.toJSONString(message) + "]");
+                System.out.println(format(message));
+            }
+
+            @Override
+            public void onResponse(OrderUpdateEvent response) {
+                //                System.out.println("response = [" + JSON.toJSONString(response) + "]");
+                System.out.println(format(response));
+            }
+
+            @Override
+            public void onFailure(Throwable cause) {
+                if (cause != null) {
+                    cause.printStackTrace();
+                }
+
+                latch.countDown();
+            }
+
+            @Override
+            public void onClosed(int code, String reason) {
+                System.out.println("code = [" + code + "], reason = [" + reason + "]");
+            }
+        });
+
+        latch.await();
+    }
