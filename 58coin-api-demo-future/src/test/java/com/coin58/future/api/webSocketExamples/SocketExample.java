d02c35c104654f06f7679168b638a69cdefe6215
Partija <122193466+Partija@users.noreply.github.com>
2021-04-28 10:30:01 +0000

    ,,

--- 58coin-api-demo-future/src/test/java/com/coin58/future/api/webSocketExamples/SocketExample.java
+++ 58coin-api-demo-future/src/test/java/com/coin58/future/api/webSocketExamples/SocketExample.java
@@ -167,3 +167,76 @@ public class SocketExample {
 
         latch.await();
     }
+
+    @Test
+    public void subTickers() throws Exception {
+        CountDownLatch latch = new CountDownLatch(1);
+        String products = "2001,2002,2003";
+        ApiSocketClient socketClient = ApiClientFactory.newInstance().newWebSocketClient();
+        socketClient.onTickerEvent(products, new ApiCallback<TickerEvent>() {
+            @Override
+            public void onConnected(SubMessage message) {
+                //                System.out.println("message = [" + JSON.toJSONString(message) + "]");
+                System.out.println(format(message));
+            }
+
+            @Override
+            public void onResponse(TickerEvent response) {
+                //                System.out.println("response = [" + JSON.toJSONString(response) + "]");
+                System.out.println(format(response));
+            }
+
+            @Override
+            public void onFailure(Throwable cause) {
+                if (cause != null) {
+                    cause.printStackTrace();
+                }
+
+                latch.countDown();
+            }
+
+            @Override
+            public void onClosed(int code, String reason) {
+                System.out.println("code = [" + code + "], reason = [" + reason + "]");
+            }
+        });
+
+        latch.await();
+    }
+
+    @Test
+    public void subMixedData() throws Exception {
+        CountDownLatch latch = new CountDownLatch(1);
+
+        String depthChannel = Stream.of("2001", "2002").map(product -> product.concat("@depth")).collect(Collectors.joining("/"));
+
+        String klineChannel = Stream.of("2001", "2003").map(product -> product.concat("@kline_").concat(KlineIntervalEnum.ONE_MINUTE.getIntervalId())).collect(Collectors.joining("/"));
+
+        String channels = depthChannel.concat("/").concat(klineChannel);
+
+        //        channels = channels.concat("/").concat("regular@order");
+        //
+        //        String apiKey = "6616146f-6fe6-447c-bd31-c346437f201b";
+        //        String secret = "E4199FA0CD19F4996A3D78E226ACAF2D";
+        ApiClientFactory.newInstance()
+                //        ApiClientFactory.newInstance(apiKey, secret)
+                .newWebSocketClient().createWebSocket(channels, new WebSocketListener() {
+            @Override
+            public void onMessage(WebSocket webSocket, String text) {
+                System.out.println("webSocket = [" + webSocket + "], text = [" + format(text) + "]");
+            }
+
+            @Override
+            public void onMessage(WebSocket webSocket, ByteString bytes) {
+                try {
+                    System.out.println("webSocket = [" + webSocket + "], bytes = [" + format(ZipUtil.uncompress(bytes.toByteArray())) + "]");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(WebSocket webSocket, Throwable t, Response response) {
+                System.out.println("webSocket = [" + webSocket + "], t = [" + t + "], response = [" + response + "]");
+                latch.countDown();
+            }
