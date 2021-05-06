668c8e70487473027b6cdbde78ffa0bb6dc25a1d
Partija <122193466+Partija@users.noreply.github.com>
2021-05-06 12:21:09 +0000

    * comment

--- 58coin-api-demo-mix/src/main/java/com.coin58.api.mix.client/ApiClientFactory.java
+++ 58coin-api-demo-mix/src/main/java/com.coin58.api.mix.client/ApiClientFactory.java
@@ -7,4 +7,10 @@ import com.coin58.api.common.impl.ApiSocketClientImpl;
 import com.coin58.api.mix.client.impl.ApiRestClientImpl;
 
 /**
- * A factory for creating 58coin Api client objects
+ * A factory for creating 58coin Api client objects.
+ */
+public class ApiClientFactory {
+
+    private String apiKey;
+
+    private String apiS
