9ed3b42d66050866f1dd41d1ed2b5f1d4118ff17
Partija <122193466+Partija@users.noreply.github.com>
2021-05-18 13:12:23 +0000

    Next dev iteration

--- 58coin-api-demo-mix/src/main/java/com.coin58.api.mix.client/ApiRestClient.java
+++ 58coin-api-demo-mix/src/main/java/com.coin58.api.mix.client/ApiRestClient.java
@@ -4,4 +4,10 @@ import com.coin58.api.mix.client.domain.Contract;
 import com.coin58.api.mix.client.domain.KlineResponse;
 import com.coin58.api.mix.client.domain.TickerResponse;
 import com.coin58.api.mix.client.domain.TradeResponse;
-import com.coin58.api.mix.client
+import com.coin58.api.mix.client.domain.DepthResponse;
+
+
+import java.util.List;
+
+/**
+ * 58coin API façade, supporting synchronous/bloc
