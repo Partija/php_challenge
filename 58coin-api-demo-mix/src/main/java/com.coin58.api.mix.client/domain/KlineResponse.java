8ee64c0930c5560df7f6d934a9ad65469af7b056
Partija <122193466+Partija@users.noreply.github.com>
2021-05-21 15:33:56 +0000

    Delete unused file.

--- 58coin-api-demo-mix/src/main/java/com.coin58.api.mix.client/domain/KlineResponse.java
+++ 58coin-api-demo-mix/src/main/java/com.coin58.api.mix.client/domain/KlineResponse.java
@@ -5,4 +5,11 @@ import lombok.Data;
 /**
  * @author lhc
  * @version 1.0
- * @date 2019/3/14 
+ * @date 2019/3/14 19:55
+ * @description
+ * @modify
+ */
+@Data
+public class KlineResponse {
+
+    private Str
