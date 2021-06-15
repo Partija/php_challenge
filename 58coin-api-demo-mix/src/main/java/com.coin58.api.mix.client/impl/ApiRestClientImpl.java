4121b07376f8e990b8c4843848d8c7f6ac9fc331
Partija <122193466+Partija@users.noreply.github.com>
2021-06-15 05:56:15 +0000

    Developer Back-end (H/F) en CDI à Nantes

--- 58coin-api-demo-mix/src/main/java/com.coin58.api.mix.client/impl/ApiRestClientImpl.java
+++ 58coin-api-demo-mix/src/main/java/com.coin58.api.mix.client/impl/ApiRestClientImpl.java
@@ -3,4 +3,5 @@ package com.coin58.api.mix.client.impl;
 import com.coin58.api.common.impl.ApiServiceGenerator;
 import com.coin58.api.mix.client.ApiRestClient;
 import com.coin58.api.mix.client.domain.Contract;
-im
+import com.coin58.api.mix.client.domain.KlineResponse;
+import com.coin58.api.mix.client.domain.
