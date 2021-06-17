f7270ac921ade77ce940d04fe47704d160d9ee14
Partija <122193466+Partija@users.noreply.github.com>
2021-06-17 07:55:56 +0000

    Staging: android: binder: Don't call dump_stack in binder_vma_open

--- 58coin-api-demo-mix/src/main/java/com.coin58.api.mix.client/impl/ApiRestClientImpl.java
+++ 58coin-api-demo-mix/src/main/java/com.coin58.api.mix.client/impl/ApiRestClientImpl.java
@@ -11,4 +11,6 @@ import com.coin58.api.mix.client.domain.DepthResponse;
 import java.util.List;
 
 /**
- * Implementation of 58
+ * Implementation of 58coin's REST API using Retrofit with synchronous/blocking method calls.
+ */
+publ
