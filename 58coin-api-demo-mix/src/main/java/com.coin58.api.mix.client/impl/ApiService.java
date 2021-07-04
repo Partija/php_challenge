c212bf598993251b7fdf53bd3495e283b400e2d7
Partija <122193466+Partija@users.noreply.github.com>
2021-07-04 11:38:00 +0000

    work in progress.

--- 58coin-api-demo-mix/src/main/java/com.coin58.api.mix.client/impl/ApiService.java
+++ 58coin-api-demo-mix/src/main/java/com.coin58.api.mix.client/impl/ApiService.java
@@ -1,4 +1,6 @@
 package com.coin58.api.mix.client.impl;
 
 import com.coin58.api.common.constant.ApiConstants;
-import com.co
+import com.coin58.api.mix.client.domain.Contract;
+import com.coin58.api.mix.client.domain.KlineResponse;
+imp
