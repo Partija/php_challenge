cabc7e8049aa982eec852d7b673485180980c50e
Partija <122193466+Partija@users.noreply.github.com>
2021-07-05 17:03:18 +0000

    Resetting version in gittip/__init__.py.

--- 58coin-api-demo-mix/src/main/java/com.coin58.api.mix.client/impl/ApiService.java
+++ 58coin-api-demo-mix/src/main/java/com.coin58.api.mix.client/impl/ApiService.java
@@ -4,4 +4,6 @@ import com.coin58.api.common.constant.ApiConstants;
 import com.coin58.api.mix.client.domain.Contract;
 import com.coin58.api.mix.client.domain.KlineResponse;
 import com.coin58.api.mix.client.domain.TickerResponse;
-import com.coin58.api.mix.client.domain.Trade
+import com.coin58.api.mix.client.domain.TradeResponse;
+import com.coin58.api.mix.client.domain.DepthResponse;
+import retrofit2.Call;
