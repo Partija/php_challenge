29a34b65dd421e989b1afd41445fe8458e75af35
Partija <122193466+Partija@users.noreply.github.com>
2021-07-05 10:50:35 +0000

    extend readme

--- 58coin-api-demo-mix/src/main/java/com.coin58.api.mix.client/impl/ApiService.java
+++ 58coin-api-demo-mix/src/main/java/com.coin58.api.mix.client/impl/ApiService.java
@@ -3,4 +3,5 @@ package com.coin58.api.mix.client.impl;
 import com.coin58.api.common.constant.ApiConstants;
 import com.coin58.api.mix.client.domain.Contract;
 import com.coin58.api.mix.client.domain.KlineResponse;
-imp
+import com.coin58.api.mix.client.domain.TickerResponse;
+import com.coin58.api.mix.client.domain.Trade
