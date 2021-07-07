650aff7279239987d7596f80676e376a66899b68
Partija <122193466+Partija@users.noreply.github.com>
2021-07-07 15:02:01 +0000

    annotations

--- 58coin-api-demo-mix/src/main/java/com.coin58.api.mix.client/impl/ApiService.java
+++ 58coin-api-demo-mix/src/main/java/com.coin58.api.mix.client/impl/ApiService.java
@@ -7,3 +7,9 @@ import com.coin58.api.mix.client.domain.TickerResponse;
 import com.coin58.api.mix.client.domain.TradeResponse;
 import com.coin58.api.mix.client.domain.DepthResponse;
 import retrofit2.Call;
+import retrofit2.http.*;
+
+import java.util.List;
+
+/**
+ * 58coin's REST API URL mappings and end
