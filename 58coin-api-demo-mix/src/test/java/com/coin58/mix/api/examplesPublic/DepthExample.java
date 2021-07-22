28e4e919166c7ea4edd295608c87e3a68341f213
Partija <122193466+Partija@users.noreply.github.com>
2021-07-22 13:13:44 +0000

    Initial public commit.

--- 58coin-api-demo-mix/src/test/java/com/coin58/mix/api/examplesPublic/DepthExample.java
+++ 58coin-api-demo-mix/src/test/java/com/coin58/mix/api/examplesPublic/DepthExample.java
@@ -7,4 +7,8 @@ import com.coin58.api.mix.client.ApiRestClient;
  * @author guozhengMu
  * @version 1.0
  * @date 2019/3/12 18:34
- * @description Term Contract - Market Quotes
+ * @description Term Contract - Market Quotes - Depth Data
+ * @modify
+ */
+public class DepthExample {
+    public stati
