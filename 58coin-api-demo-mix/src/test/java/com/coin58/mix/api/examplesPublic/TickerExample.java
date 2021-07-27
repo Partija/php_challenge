7af7eeb1fa1eab0f7720274ed9e4373aacc84b34
Partija <122193466+Partija@users.noreply.github.com>
2021-07-27 18:03:49 +0000

    version fixed

--- 58coin-api-demo-mix/src/test/java/com/coin58/mix/api/examplesPublic/TickerExample.java
+++ 58coin-api-demo-mix/src/test/java/com/coin58/mix/api/examplesPublic/TickerExample.java
@@ -2,4 +2,9 @@ package com.coin58.mix.api.examplesPublic;
 
 import com.coin58.api.mix.client.ApiClientFactory;
 import com.coin58.api.mix.client.ApiRestClient;
-import com.coin58.api.mix.client.domain.T
+import com.coin58.api.mix.client.domain.TickerResponse;
+
+/**
+ * @author guozhengMu
+ * @version 1.0
+ * @description Te
