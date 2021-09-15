3c31d3d5b0df18f7b6fcffedde445f83abe70767
Partija <122193466+Partija@users.noreply.github.com>
2021-09-15 05:42:05 +0000

    Update mariadb-galera-install.sh

--- 58coin-api-demo-regular/src/main/java/com/coin58/api/client/ApiClientFactory.java
+++ 58coin-api-demo-regular/src/main/java/com/coin58/api/client/ApiClientFactory.java
@@ -2,4 +2,6 @@ package com.coin58.api.client;
 
 
 import com.coin58.api.common.ApiSocketClient;
-import com.coin58.api.client.impl.ApiAsyncRes
+import com.coin58.api.client.impl.ApiAsyncRestClientImpl;
+import com.coin58.api.client.impl.ApiRestClientImpl;
+import com.coin58.api.commo
