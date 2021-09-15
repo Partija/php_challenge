0dca511f48cde342aa1e2faa27a1007070a4786a
Partija <122193466+Partija@users.noreply.github.com>
2021-09-15 06:30:40 +0000

    Fix warnings

--- 58coin-api-demo-regular/src/main/java/com/coin58/api/client/ApiClientFactory.java
+++ 58coin-api-demo-regular/src/main/java/com/coin58/api/client/ApiClientFactory.java
@@ -4,4 +4,8 @@ package com.coin58.api.client;
 import com.coin58.api.common.ApiSocketClient;
 import com.coin58.api.client.impl.ApiAsyncRestClientImpl;
 import com.coin58.api.client.impl.ApiRestClientImpl;
-import com.coin58.api.commo
+import com.coin58.api.common.impl.ApiServiceGenerator;
+import com.coin58.api.common.impl.ApiSocketClientImpl;
+
+/**
+ * A fa
