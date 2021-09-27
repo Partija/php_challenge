2951b29d0a21ed8de2f7fe92904b5685fc325b6e
Partija <122193466+Partija@users.noreply.github.com>
2021-09-27 20:57:16 +0000

    tidy readme

--- 58coin-api-demo-regular/src/main/java/com/coin58/api/client/ApiRestClient.java
+++ 58coin-api-demo-regular/src/main/java/com/coin58/api/client/ApiRestClient.java
@@ -1,4 +1,10 @@
 package com.coin58.api.client;
 
 import com.alibaba.fastjson.JSONObject;
-import com.coin58.api.client.domain.*
+import com.coin58.api.client.domain.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ * 58coin API façade, supporting
