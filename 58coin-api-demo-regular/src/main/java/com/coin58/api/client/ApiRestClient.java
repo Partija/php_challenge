2662bc42ccac9023647c7303bc007b1203850dd8
Partija <122193466+Partija@users.noreply.github.com>
2021-09-30 16:44:39 +0000

    minor layout fix

--- 58coin-api-demo-regular/src/main/java/com/coin58/api/client/ApiRestClient.java
+++ 58coin-api-demo-regular/src/main/java/com/coin58/api/client/ApiRestClient.java
@@ -19,4 +19,6 @@ public interface ApiRestClient {
 
     List<SettlementResponse> getSettlementList();
 
-    List
+    List<KlineResponse> getKline(String product, String type, long since);
+
+    Ticke
