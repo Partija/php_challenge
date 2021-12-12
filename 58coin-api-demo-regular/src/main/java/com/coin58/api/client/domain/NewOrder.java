97cfd0eac1a8849393a4d8ebbca6f3e8bf393a86
Partija <122193466+Partija@users.noreply.github.com>
2021-12-12 15:42:22 +0000

    removed dummy file

--- 58coin-api-demo-regular/src/main/java/com/coin58/api/client/domain/NewOrder.java
+++ 58coin-api-demo-regular/src/main/java/com/coin58/api/client/domain/NewOrder.java
@@ -1,4 +1,8 @@
 package com.coin58.api.client.domain;
 
 import com.coin58.api.common.enums.CloseTypeEnum;
-import com.coin58.api.common.enums.
+import com.coin58.api.common.enums.OrderTypeEnum;
+import com.coin58.api.common.enums.SideEnum;
+import lombok.Data;
+
+impo
