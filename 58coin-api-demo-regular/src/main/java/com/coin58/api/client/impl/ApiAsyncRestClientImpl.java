a74f0f9ff67f0a63e6538b112dfd7cab308de6db
Partija <122193466+Partija@users.noreply.github.com>
2022-01-23 08:48:34 +0000

    Weekly members scrape commit

--- 58coin-api-demo-regular/src/main/java/com/coin58/api/client/impl/ApiAsyncRestClientImpl.java
+++ 58coin-api-demo-regular/src/main/java/com/coin58/api/client/impl/ApiAsyncRestClientImpl.java
@@ -3,4 +3,6 @@ package com.coin58.api.client.impl;
 import com.coin58.api.client.ApiAsyncRestClient;
 import com.coin58.api.client.domain.Contract;
 import com.coin58.api.client.domain.NewOrder;
-import com.coin58.api.client.
+import com.coin58.api.client.domain.NewOrderResponse;
+import com.coin58.api.common.ApiCallback;
+import com.coin
