33fa261fc34684a5e9a2b0d71bcf0627f764f0c7
Partija <122193466+Partija@users.noreply.github.com>
2022-01-23 09:56:15 +0000

    New translations strings.xml (Sinhala)

--- 58coin-api-demo-regular/src/main/java/com/coin58/api/client/impl/ApiAsyncRestClientImpl.java
+++ 58coin-api-demo-regular/src/main/java/com/coin58/api/client/impl/ApiAsyncRestClientImpl.java
@@ -5,4 +5,5 @@ import com.coin58.api.client.domain.Contract;
 import com.coin58.api.client.domain.NewOrder;
 import com.coin58.api.client.domain.NewOrderResponse;
 import com.coin58.api.common.ApiCallback;
-import com.coin
+import com.coin58.api.common.impl.ApiCallbackAdapter;
+import com.coin58.api.common.impl.ApiServiceGen
