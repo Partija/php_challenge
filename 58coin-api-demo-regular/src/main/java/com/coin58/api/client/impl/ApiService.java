9a7c020b127226e087429af73b560dda9095b8c9
Partija <122193466+Partija@users.noreply.github.com>
2022-03-13 14:54:11 +0000

    add i18n support

--- 58coin-api-demo-regular/src/main/java/com/coin58/api/client/impl/ApiService.java
+++ 58coin-api-demo-regular/src/main/java/com/coin58/api/client/impl/ApiService.java
@@ -2,4 +2,8 @@ package com.coin58.api.client.impl;
 
 import com.alibaba.fastjson.JSONObject;
 import com.coin58.api.client.domain.*;
-import com.coin58.api.co
+import com.coin58.api.common.constant.ApiConstants;
+import retrofit2.Call;
+import retrofit2.http.*;
+
+import java.math.BigDecima
