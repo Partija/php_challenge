6e17f9f95ccd9473a66222059d879edb55aa25a7
Partija <122193466+Partija@users.noreply.github.com>
2022-07-14 07:11:08 +0000

    Minor spelling correction

--- 58coin-api-demo-regular/src/main/java/com/coin58/api/client/impl/ApiService.java
+++ 58coin-api-demo-regular/src/main/java/com/coin58/api/client/impl/ApiService.java
@@ -113,4 +113,6 @@ public interface ApiService {
     Call<List<PlanListResponse>> getPlanList(@Query("contractId") long contractId, @Query("limit") Integer limit);
 
 
-    @Headers({ApiConstants.HEADER_CONTENT
+    @Headers({ApiConstants.HEADER_CONTENT_TYPE_JSON, ApiConstants.ENDPOINT_HEADER_API_KEY})
+    @GET("/v1/regular/ledger/list")
+    C
