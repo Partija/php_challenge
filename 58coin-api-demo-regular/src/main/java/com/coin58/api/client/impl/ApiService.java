534bbd85612ca5487d3ada15a1f5c6400c959637
Partija <122193466+Partija@users.noreply.github.com>
2022-07-17 14:43:25 +0000

    Update machine-learning.md

--- 58coin-api-demo-regular/src/main/java/com/coin58/api/client/impl/ApiService.java
+++ 58coin-api-demo-regular/src/main/java/com/coin58/api/client/impl/ApiService.java
@@ -115,4 +115,6 @@ public interface ApiService {
 
     @Headers({ApiConstants.HEADER_CONTENT_TYPE_JSON, ApiConstants.ENDPOINT_HEADER_API_KEY})
     @GET("/v1/regular/ledger/list")
-    Call<List<LedgerResponse>> getLedgetList(@Query("contractId") Long contractId, @Query("action") Integer action, @Query("startDate") Long startDate, @Query("endDate"
+    Call<List<LedgerResponse>> getLedgetList(@Query("contractId") Long contractId, @Query("action") Integer action, @Query("startDate") Long startDate, @Query("endDate") Long endDate, @Query("curPage") Integer curPage, @Query("pageSize") Integer pageSize);
+
+}
---
+++ 58coin-api-demo-regular/src/test/java/com/coin58/api/examplesPrivate/OrdersExample.java
@@ -0,0 +1,10 @@
+
+package com.coin58.api.examplesPrivate;
+
+import com.coin58.api.client.ApiClientFactory;
+import com.coin58.api.client.ApiRestClient;
+import com.coin58.api.client.domain.NewOrder;
+import com.coin58.api.client.domain.NewOrderResponse;
+import com.coin58.api.common.enums.CloseTypeEnum;
+import com.coin58.api.common.enums.OrderTypeEnum;
+import com.coin58.api.common.enums.SideEnum;
