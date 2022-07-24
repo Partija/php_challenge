d401367fb51501ab3a19ea1b1e7eb435a9997276
Partija <122193466+Partija@users.noreply.github.com>
2022-07-24 21:43:29 +0000

    Comment-only change.

--- 58coin-api-demo-regular/src/test/java/com/coin58/api/examplesPrivate/SingatureExample.java
+++ 58coin-api-demo-regular/src/test/java/com/coin58/api/examplesPrivate/SingatureExample.java
@@ -7,4 +7,7 @@ import java.util.Map;
 
 import static com.coin58.api.common.security.SignatureUtil.createSignature;
 
-public class SingatureExample
+public class SingatureExample {
+
+    private final String apiKey = "089cf604-7b87-4b13-b806-eaadb67c8b70";
+    pr
