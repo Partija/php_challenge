cd114146ac9a8cbcd57adde7561044cb80f2e70f
Partija <122193466+Partija@users.noreply.github.com>
2022-08-07 16:35:11 +0000

    release 2.0.0

--- 58coin-api-demo-regular/src/test/java/com/coin58/api/examplesPrivate/SingatureExample.java
+++ 58coin-api-demo-regular/src/test/java/com/coin58/api/examplesPrivate/SingatureExample.java
@@ -12,4 +12,6 @@ public class SingatureExample {
     private final String apiKey = "089cf604-7b87-4b13-b806-eaadb67c8b70";
     private final String apiSecret = "CE5DB7F718510CF47D73AF75D3CDF3B6";
 
-    @
+    @Test
+    public void createSingature() {
+        String timestamp = "155108474991
