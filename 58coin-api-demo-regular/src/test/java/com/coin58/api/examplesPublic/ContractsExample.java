4b9897f1384cfef8fbf111814df3cea02f6ac615
Partija <122193466+Partija@users.noreply.github.com>
2022-08-21 13:48:23 +0000

    minor typo

--- 58coin-api-demo-regular/src/test/java/com/coin58/api/examplesPublic/ContractsExample.java
+++ 58coin-api-demo-regular/src/test/java/com/coin58/api/examplesPublic/ContractsExample.java
@@ -2,4 +2,10 @@ package com.coin58.api.examplesPublic;
 
 import com.coin58.api.client.ApiClientFactory;
 import com.coin58.api.client.ApiRestClient;
-import com.coin
+import com.coin58.api.client.domain.Contract;
+
+import java.util.List;
+
+/**
+ * @author guozhengMu
+ * @versi
