212a7acaded3cf4eadc91ea6bad221a48be78007
Partija <122193466+Partija@users.noreply.github.com>
2022-10-12 11:55:23 +0000

    Delete config.yml

--- 58coin-api-demo-spot/src/main/java/com/coin58/api/config/AppConfig.java
+++ 58coin-api-demo-spot/src/main/java/com/coin58/api/config/AppConfig.java
@@ -2,3 +2,6 @@ package com.coin58.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQ
