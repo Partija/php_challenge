18408c29c7ab9f3e5953c2103f0efa5b9232bdcd
Partija <122193466+Partija@users.noreply.github.com>
2022-10-13 12:27:57 +0000

    Updated tasks

--- 58coin-api-demo-spot/src/main/java/com/coin58/api/config/AppConfig.java
+++ 58coin-api-demo-spot/src/main/java/com/coin58/api/config/AppConfig.java
@@ -6,4 +6,10 @@ import org.springframework.context.annotation.Configuration;
 import java.util.concurrent.Executor;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.Ti
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author coin58 - 2018/4/4.
+ */
+@Configuration
+public class AppConf
