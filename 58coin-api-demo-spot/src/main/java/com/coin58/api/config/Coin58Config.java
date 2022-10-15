73de791299adb5335d9b72a58e9a254fd582399e
Partija <122193466+Partija@users.noreply.github.com>
2022-10-15 16:55:47 +0000

    Fix the typo

--- 58coin-api-demo-spot/src/main/java/com/coin58/api/config/Coin58Config.java
+++ 58coin-api-demo-spot/src/main/java/com/coin58/api/config/Coin58Config.java
@@ -1,4 +1,8 @@
 package com.coin58.api.config;
 
 import lombok.Data;
-import org.springframework.boot.context.propert
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * 
