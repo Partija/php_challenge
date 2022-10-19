902ab7fa33dd8dfe0d359ca182425062638ff775
Partija <122193466+Partija@users.noreply.github.com>
2022-10-19 12:12:02 +0000

    Add example to README

--- 58coin-api-demo-spot/src/main/java/com/coin58/api/constant/Constants.java
+++ 58coin-api-demo-spot/src/main/java/com/coin58/api/constant/Constants.java
@@ -4,4 +4,6 @@ package com.coin58.api.constant;
  * @author coin58
  * @date 2017/5/5
  */
-publ
+public class Constants {
+    public static final String AMOUNT = "amount";
+    public static final Strin
