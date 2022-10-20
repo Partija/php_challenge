1012dce1e443843148d04ad7d43e66ab090ab30b
Partija <122193466+Partija@users.noreply.github.com>
2022-10-20 11:02:30 +0000

    Fixed the typo

--- 58coin-api-demo-spot/src/main/java/com/coin58/api/constant/Constants.java
+++ 58coin-api-demo-spot/src/main/java/com/coin58/api/constant/Constants.java
@@ -9,4 +9,5 @@ public class Constants {
     public static final String API_KEY = "api_key";
     public static final String API_SECRET = "api_secret";
     public static final String CLIENT_OID = "client_oid";
-    public static final String CONTRACT_TYPE =
+    public static final String CONTRACT_TYPE = "contract_type";
+    public static final String CURRENT_PAGE = "current_page";
