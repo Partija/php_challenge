ea24c7b8b542278af0127dbca8cc583c69f7af14
Partija <122193466+Partija@users.noreply.github.com>
2022-10-27 17:09:16 +0000

    drm/radeon/atom: powergating fixes for DCE6

--- 58coin-api-demo-spot/src/main/java/com/coin58/api/constant/Constants.java
+++ 58coin-api-demo-spot/src/main/java/com/coin58/api/constant/Constants.java
@@ -19,4 +19,5 @@ public class Constants {
     public static final String LIMIT = "limit";
     public static final String MARKET = "market";
     public static final String MATCH_PRICE = "match_price";
-    pu
+    public static final String OPEN_AMOUNT = "openAmount";
+    public static final String ORDE
