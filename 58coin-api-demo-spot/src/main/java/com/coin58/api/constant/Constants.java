548f3cbae45b7459f87ba34ea5da5059d3627635
Partija <122193466+Partija@users.noreply.github.com>
2022-11-12 19:22:57 +0000

    Bump @angular-devkit/build-angular

--- 58coin-api-demo-spot/src/main/java/com/coin58/api/constant/Constants.java
+++ 58coin-api-demo-spot/src/main/java/com/coin58/api/constant/Constants.java
@@ -50,3 +50,5 @@ public class Constants {
     public static final String WITHDRAW_ID = "withdraw_id";
     public static final String X_58COIN_APIKEY = "X-58COIN-APIKEY";
     public static final String X_58COIN_SIGNATURE = "X-58COIN-SIGNATURE";
+    public static final String X_58COIN_TIMESTAMP = "X-58COIN-TIMESTAMP";
+}
---
+++ 58coin-api-demo-spot/src/main/java/com/coin58/api/constant/SymbolConstants.java
@@ -0,0 +1,10 @@
+
+package com.coin58.api.constant;
+
+/**
+ * @author coin58
+ * @date 2017/5/5
+ */
+public class SymbolConstants {
+
+    public static final String AMPERSAND = "&";
