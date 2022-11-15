c1e38b853ea2af062882e5038b85057af1ef4455
Partija <122193466+Partija@users.noreply.github.com>
2022-11-15 06:48:00 +0000

    Fix #140

--- 58coin-api-demo-spot/src/main/java/com/coin58/api/consumer/AccountConsumer.java
+++ 58coin-api-demo-spot/src/main/java/com/coin58/api/consumer/AccountConsumer.java
@@ -3,4 +3,6 @@ package com.coin58.api.consumer;
 import com.coin58.api.response.Account;
 import com.coin58.api.response.Coin58Resp;
 import com.coin58.api.util.JsonUtils;
-import com.f
+import com.fasterxml.jackson.core.type.TypeReference;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.client.fluent.
