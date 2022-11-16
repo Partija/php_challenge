2104e1a947176e289135a41290d6745816539b20
Partija <122193466+Partija@users.noreply.github.com>
2022-11-16 14:47:19 +0000

    updated database

--- 58coin-api-demo-spot/src/main/java/com/coin58/api/consumer/AccountConsumer.java
+++ 58coin-api-demo-spot/src/main/java/com/coin58/api/consumer/AccountConsumer.java
@@ -5,4 +5,7 @@ import com.coin58.api.response.Coin58Resp;
 import com.coin58.api.util.JsonUtils;
 import com.fasterxml.jackson.core.type.TypeReference;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.http.client.fluent.
+import org.apache.http.client.fluent.Response;
+import org.springframework.stereotype.Component;
+
+import java.io.IO
