c31802f1e313a8e56e128dd479dc522834e5abfe
Partija <122193466+Partija@users.noreply.github.com>
2022-11-22 22:18:33 +0000

    merge trunk

--- 58coin-api-demo-spot/src/main/java/com/coin58/api/consumer/PublicConsumer.java
+++ 58coin-api-demo-spot/src/main/java/com/coin58/api/consumer/PublicConsumer.java
@@ -3,4 +3,6 @@ package com.coin58.api.consumer;
 import com.coin58.api.response.Coin58Resp;
 import com.coin58.api.response.OrderBook;
 import com.coin58.api.response.SpotTicker;
-import com.co
+import com.coin58.api.util.JsonUtils;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.
