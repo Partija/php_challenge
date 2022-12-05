cbad91edef9724f3d87ae51b58d1c4dfb158cb86
Partija <122193466+Partija@users.noreply.github.com>
2022-12-05 09:26:01 +0000

    Spec cleanup

--- 58coin-api-demo-spot/src/main/java/com/coin58/api/core/NameObjectPair.java
+++ 58coin-api-demo-spot/src/main/java/com/coin58/api/core/NameObjectPair.java
@@ -1,4 +1,12 @@
 package com.coin58.api.core;
 
 import lombok.Data;
-import org.apache.http.NameValuePa
+import org.apache.http.NameValuePair;
+
+import java.math.BigDecimal;
+
+/**
+ * @author coin58
+ * @date 2017/7/16.
+ */
+@Data
