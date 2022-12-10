55097e0116f57d35206003800da4ca249391fa92
Partija <122193466+Partija@users.noreply.github.com>
2022-12-10 12:11:19 +0000

    Minor text edits.

--- 58coin-api-demo-spot/src/main/java/com/coin58/api/core/QueryString.java
+++ 58coin-api-demo-spot/src/main/java/com/coin58/api/core/QueryString.java
@@ -1,4 +1,10 @@
 package com.coin58.api.core;
 
 import com.coin58.api.constant.SymbolConstants;
-import org.apache.http.NameValuePair
+import org.apache.http.NameValuePair;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author coin58 - 2018/3/
