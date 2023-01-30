efb81eac165a2338268483ae343817fc041fe5b0
Partija <122193466+Partija@users.noreply.github.com>
2023-01-30 10:51:55 +0000

    More error handling

--- 58coin-api-demo-spot/src/main/java/com/coin58/api/response/Order.java
+++ 58coin-api-demo-spot/src/main/java/com/coin58/api/response/Order.java
@@ -32,4 +32,7 @@ public class Order {
     private String orderId;
     @JsonProperty("client_oid")
     private String clientOid;
-    p
+    private String symbol;
+    private String type;
+    private String side;
+    private Strin
