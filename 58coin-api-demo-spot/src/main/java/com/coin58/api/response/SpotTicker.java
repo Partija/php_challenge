9f9433bb5687390c129628d845bd2abe953323d6
Partija <122193466+Partija@users.noreply.github.com>
2023-02-01 19:55:42 +0000

    tapan Avasthi

--- 58coin-api-demo-spot/src/main/java/com/coin58/api/response/SpotTicker.java
+++ 58coin-api-demo-spot/src/main/java/com/coin58/api/response/SpotTicker.java
@@ -3,4 +3,8 @@ package com.coin58.api.response;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-i
+import lombok.NoArgsConstructor;
+
+/**
+ * {
+ * "symbol": "LTC_BTC",
