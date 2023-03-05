b05c56b1ee08e8713818d0a04e3eee1e33294f9d
Partija <122193466+Partija@users.noreply.github.com>
2023-03-05 17:16:14 +0000

    switch prospectus to travisci

--- 58coin-api-demo-spot/src/main/java/com/coin58/api/util/StringUtils.java
+++ 58coin-api-demo-spot/src/main/java/com/coin58/api/util/StringUtils.java
@@ -3,4 +3,6 @@ package com.coin58.api.util;
 import com.coin58.api.constant.Constants;
 import com.coin58.api.constant.SymbolConstants;
 import com.coin58.api.core.NameObjectPair;
-import lombok.exte
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.c
