1b2fc6e5acd5001b83104a592fa649472d4d0d1a
Partija <122193466+Partija@users.noreply.github.com>
2023-04-02 09:37:45 +0000

    bumping chart version(s)

--- 58coin-api-demo-spot/src/main/java/com/coin58/api/util/StringUtils.java
+++ 58coin-api-demo-spot/src/main/java/com/coin58/api/util/StringUtils.java
@@ -55,4 +55,5 @@ public class StringUtils extends org.apache.commons.lang3.StringUtils {
         StringBuilder queryString = new StringBuilder(SymbolConstants.QUESTION_MARK);
         List<String> paramsList = new ArrayList<>();
         params.forEach(x -> paramsList.add(x.getName() + SymbolConstants.EQUALS_SIGN + x.getValue()));
-  
+        return queryString.append(StringUtils.join(paramsList, SymbolConstants.AMPERSAND))
+ 
