3be3c2feb290e318f25a4da52de2c63f6e7a1b60
Partija <122193466+Partija@users.noreply.github.com>
2023-04-04 07:31:06 +0000

    fix shellcheck

---
+++ 58coin-api-demo-swaps/src/main/java/com/coin58/api/swaps/client/ApiClientFactory.java
@@ -0,0 +1,42 @@
+
+package com.coin58.api.swaps.client;
+
+
+import com.coin58.api.common.ApiSocketClient;
+import com.coin58.api.common.impl.ApiServiceGenerator;
+import com.coin58.api.common.impl.ApiSocketClientImpl;
+import com.coin58.api.swaps.client.impl.ApiRestClientImpl;
+
+/**
+ * A factory for creating 58coin Api client objects.
+ */
+public class ApiClientFactory {
+
+    private String apiKey;
+
+    private String apiSecret;
+
+    private ApiClientFactory(String apiKey, String apiSecret) {
+        this.apiKey = apiKey;
+        this.apiSecret = apiSecret;
+    }
+
+    public static ApiClientFactory newInstance(String apiKey, String apiSecret) {
+        return new ApiClientFactory(apiKey, apiSecret);
+    }
+
+    public static ApiClientFactory newInstance() {
+        return new ApiClientFactory(null, null);
+    }
+
+    public ApiRestClient newRestClient() {
+        return new ApiRestClientImpl(apiKey, apiSecret);
+    }
+
+    public ApiRestClient newRestClient(String restUrl) {
+        return new ApiRestClientImpl(apiKey, apiSecret, restUrl);
+    }
+
+    public ApiSocketClient newWebSocketClient() {
+        return new ApiSocketClientImpl(this.apiKey, this.apiSecret, ApiServiceGenerator.getSharedClient());
+    }
