6ebc6ace829a7c11cb29d3e240e1d67675f69b58
Partija <122193466+Partija@users.noreply.github.com>
2023-04-07 23:03:31 +0000

    b43: A fix for DMA transmission sequence errors

--- 58coin-api-demo-swaps/src/main/java/com/coin58/api/swaps/client/domain/NewOrder.java
+++ 58coin-api-demo-swaps/src/main/java/com/coin58/api/swaps/client/domain/NewOrder.java
@@ -1,3 +1,5 @@
 package com.coin58.api.swaps.client.domain;
 
-import com.coin58.api.common.enums.Close
+import com.coin58.api.common.enums.CloseTypeEnum;
+import com.coin58.api.common.enums.OrderTypeEnum;
+import com.co
