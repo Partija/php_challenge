5ea7b61102b0e863ea0eb41606b21c95ca7766b6
Partija <122193466+Partija@users.noreply.github.com>
2023-04-11 16:48:05 +0000

    switching branches

--- 58coin-api-demo-swaps/src/main/java/com/coin58/api/swaps/client/domain/NewOrder.java
+++ 58coin-api-demo-swaps/src/main/java/com/coin58/api/swaps/client/domain/NewOrder.java
@@ -2,4 +2,10 @@ package com.coin58.api.swaps.client.domain;
 
 import com.coin58.api.common.enums.CloseTypeEnum;
 import com.coin58.api.common.enums.OrderTypeEnum;
-import com.co
+import com.coin58.api.common.enums.SideEnum;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+pu
